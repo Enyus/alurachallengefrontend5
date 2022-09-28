f084f6285387663c86351765f1246682b01bf0ed
Cassio Sandrigo Santini <cassio.santini@educacao.sp.gov.br>
2022-09-28 16:35:05 -0300

    adicionando noreferrer no link em dev

--- components/Dev.js
+++ components/Dev.js
@@ -7,7 +7,7 @@ import styles from '../styles/Dev.module.css';
 function Dev(props) {
     return (
         <div className={styles.dev__container}>
-            <a className={styles.dev__link} href='https://github.com/Enyus' target='_blank'>Desenvolvido por Cássio S. Santini</a>
+            <a className={styles.dev__link} href='https://github.com/Enyus' target='_blank' rel="noreferrer">Desenvolvido por Cássio S. Santini</a>
             <p className={styles.dev__text}>2022</p>
         </div>
     )
